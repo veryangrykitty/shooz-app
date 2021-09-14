2f9cfc0e78bb5aa6d6b8e873f7c7dbb9f9789d4f
royceruisheng <ruisheng.lim@gmail.com>
2021-09-14 18:34:11 +0800

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :
+  resources :listings
 
+  resources :checkout, only: [:new, :create]
+  resources :payment, only: [:create]
 
+  get '/mylistings', to: "listings#seller_all"
+  get '/listings?category=:value', to: "listings#category"
 end
