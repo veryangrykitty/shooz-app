2e1ec9cd51a5bd6c6f1f0d9cea71cd1199f14636
royceruisheng <ruisheng.lim@gmail.com>
2021-09-14 18:12:27 +0800

    modified routes for devise::user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :
+
+
 end
