e3012ce7c9e45fcc630d07acf39b0aaee2e1a910
royceruisheng <ruisheng.lim@gmail.com>
2021-09-16 15:41:25 +0800

    test again

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,8 +16,8 @@ class OrdersController < ApplicationController
           }
         }
       }],
-      success_url: order_path(order),
-      cancel_url: order_path(order)
+      success_url: order_url(order),
+      cancel_url: order_url(order)
     )
 
     order.update(checkout_session_id: session.id)
