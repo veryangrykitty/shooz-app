0df489b468a4e43f5d290f7ee4e8433b1a0d1c48
Daniel Teo <teodaniel14@gmail.com>
2021-09-16 18:24:00 +0800

    cloudinary card listing works

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_16_050533) do
-
+ActiveRecord::Schema.define(version: 2021_09_16_061458) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
