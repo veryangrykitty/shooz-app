e15e77f210696cc5436fd34487d324d5381886eb
royceruisheng <ruisheng.lim@gmail.com>
2021-09-16 14:44:48 +0800

    configured stripe. To be tested

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   def create
     listing = Listing.find(params[:id])
-    order  = Order.create!(listing: listing, sneaker_sku: listing.sneaker_model_name, amount: listing.price, state: 'pending', user: current_user)
+    order = Order.create!(listing: listing, sneaker_sku: listing.sneaker_model_name, amount: listing.price, state: 'pending', user: current_user)
 
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
@@ -11,11 +11,15 @@ class OrdersController < ApplicationController
         currency: 'sgd',
         quantity: 1
       }],
-      success_url: order_url(order),
-      cancel_url: order_url(order)
+      success_url: order_path(order),
+      cancel_url: order_path(order)
     )
 
     order.update(checkout_session_id: session.id)
     redirect_to new_order_payment_path(order)
+  end
+
+  def show
+    @order = current_user.orders.find(params[:id])
   end
 end
